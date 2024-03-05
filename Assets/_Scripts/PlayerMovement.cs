02926f188b0cfee14a7e303845066cc62ecdad70
KampfKiwi08 <arneschremmer2018@gmail.com>
2024-03-05 21:12:09 +0100

    added Player movement
    The Player can move and jump now. it also flips the sprite

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Move")]
+    [SerializeField] private float moveSpeedMultiplier;
+    [SerializeField] private float maxMoveSpeed;
+    private float horizontalInput;
+
+    [Header("Player Jump")]
+    [SerializeField] private float jumpingPower;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    private bool verticalInput;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        groundCheck = GetComponentInChildren<Transform>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        if(transform.localScale.x > 1 && horizontalInput < 0f || transform.localScale.x < 1 && horizontalInput > 0f)
+        {
+            FlipSprite();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+    }
+
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal") * moveSpeedMultiplier;
+    }
+
+    void MovePlayer()
+    {
+        Vector2 _movementDirection = new Vector2(horizontalInput, rb.velocity.y);
+        rb.velocity = _movementDirection;
+
+    }
+
+    void FlipSprite()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+}
