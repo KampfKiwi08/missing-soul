41a56fabf31e128c19ba9bb54e23d1855ffd5070
KampfKiwi08 <arneschremmer2018@gmail.com>
2024-03-08 20:36:22 +0100

    something

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -9,19 +9,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeedMultiplier;
     private float horizontalInput;
 
-    
+    [Header("Player Jump")]
+    [SerializeField] private float jumpingPower;
+    [SerializeField] private LayerMask groundLayer;
+    private Transform groundCheck;
+
+
 
     private Rigidbody2D rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        groundCheck = transform.GetChild(0).GetComponent<Transform>();
     }
 
     void Update()
     {
         GetInput();
-        if(transform.localScale.x > 1 && horizontalInput < 0f || transform.localScale.x < 1 && horizontalInput > 0f)
+        if (transform.localScale.x > 1 && horizontalInput < 0f || transform.localScale.x < 1 && horizontalInput > 0f)
         {
             FlipSprite();
         }
@@ -30,6 +36,16 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+
+        if (Input.GetButton("Jump"))
+        {
+            bool _isGrounded = IsGrounded();
+            if (_isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            }
+
+        }
     }
 
     void GetInput()
@@ -39,7 +55,7 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        
+
         Vector2 _movementDirection = new Vector2(horizontalInput, rb.velocity.y);
         rb.velocity = _movementDirection;
 
@@ -47,8 +63,11 @@ public class PlayerMovement : MonoBehaviour
 
     void FlipSprite()
     {
-        transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, 1);
     }
 
-    
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
 }
